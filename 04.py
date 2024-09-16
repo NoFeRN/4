d0eb04c33950c9353858c3e63d188299f0f08d5a
NoFeRN <114984795+NoFeRN@users.noreply.github.com>
2024-09-16 19:13:42 +0500

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,5 @@
+complete_task = 12
+hours_spent = 1.5
+name = 'Python'
+Time_for_one_task =hours_spent / complete_task
+print("Курс:",name,"всего задач:",complete_task,"затрачено часов",hours_spent,"среднее время выполнения",Time_for_one_task)
