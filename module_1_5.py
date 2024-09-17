96b0d9484b120be3210cd7bb6791e0c96deb581b
NoFeRN <114984795+NoFeRN@users.noreply.github.com>
2024-09-17 20:45:09 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1,2,3,4
+print(immutable_var)
+#immutable_var[0]= 222 кортеж не поддерживает измения элементов внутри него
+mutable_list=["комп","вода","телефон","провод"]
+print(mutable_list)
+mutable_list[1]="наушники"
+print(mutable_list)
